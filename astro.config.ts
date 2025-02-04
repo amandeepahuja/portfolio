0bffa2569c7c3b5159352102c0919ddd8461dc6e
amandeepahuja <ayeman1127@gmail.com>
2025-02-05 00:07:53 +0530

    first commit

---
+++ astro.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from 'astro/config';
+
+import expressiveCode from 'astro-expressive-code';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import spectre from './package/src';
+
+import node from '@astrojs/node';
+import { spectreDark } from './src/ec-theme';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://amandeepahuja.github.io',
+  base: '/Portfolio',
+  output: 'static',
+  integrations: [
+    expressiveCode({
+      themes: [spectreDark],
+    }),
+    mdx(),
+    sitemap(),
+    spectre({
+      name: 'Amandeep Singh',
+      openGraph: {
+        home: {
+          title: 'Amandeep Singh'
+        },
+        
+        projects: {
+          title: 'Projects'
+        }
+      }
+    })
+  ]
+});
